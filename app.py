fc92f1bf0e7511cc73af4f55fa9ccf9387d0d274
laiz-in <laizin2107@gmail.com>
2023-04-25 14:26:39 +0400

    made some changes for CSS and HTML

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import pickle
 import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
+from datetime import datetime
 
 app = Flask(__name__)
-model = pickle.load(open("ipynb_files/flight_fare.pkl", "rb"))
+model = pickle.load(open("notebook/model/random_forest_model.pkl", "rb"))
 
 
 
@@ -25,25 +26,83 @@ def predict():
 
         # Date_of_Journey
         date_dep = request.form["Dep_Time"]
-        Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
-        Journey_month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
+        Day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
+        Month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
         Dep_hour = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").hour)
-        Dep_min = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").minute)
-        date_arr = request.form["Arrival_Time"]
-        Arrival_hour = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").hour)
-        Arrival_min = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").minute)
-        Duration_hours = abs(Arrival_hour - Dep_hour)
-        Duration_mins = abs(Arrival_min - Dep_min)
-        Total_Stops = int(request.form["stops"])
+        Year = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").year)
+        Day_of_week = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").day_of_week)
+
+        
+        #classifying the time object to our departure classes
+        if Dep_hour >= 18 or Dep_hour < 6:
+            Departure = 'After 6 PM'
+        elif Dep_hour >= 12 and Dep_hour < 18:
+            Departure = '12 PM - 6 PM'
+        elif Dep_hour >= 6 and Dep_hour < 12:
+            Departure = '6 AM - 12 PM'
+        else:
+            Departure = 'Before 6 AM'
+        
+        #classifying the time object to our departure classes
+        if Dep_hour >= 18 or Dep_hour < 6:
+            Arrival = 'After 6 PM'
+        elif Dep_hour >= 12 and Dep_hour < 18:
+            Arrival = '12 PM - 6 PM'
+        elif Dep_hour >= 6 and Dep_hour < 12:
+            Arrival = '6 AM - 12 PM'
+        else:
+            Arrival = 'Before 6 AM'
+
+        # Calculate the number of days left until the departure date
+        date_dep_for = datetime.strptime(date_dep, '%Y-%m-%dT%H:%M')
+        Days_left = ((date_dep_for - datetime.now()).days)+1
+
+
+        Duration_in_hours = request.form['Duration']
+        Total_stops = (request.form["stops"])
         Airline=request.form['Airline']
+        Classes=request.form['Classes']
         Source = request.form["Source"]
         Destination = request.form["Destination"]
-        obj= DataTransformation()
-        prediction=obj.get_data_transformer(Journey_day,Journey_month,Dep_hour, Dep_min ,Arrival_hour,Arrival_min,Duration_hours,Duration_mins,Total_Stops,Airline,Source,Destination)
-        output=round(prediction[0],2)
+        df = [{
+            'Airline': Airline,
+            'Classes': Classes,
+            'Source': Source,
+            'Departure': Departure,
+            'Total_stops': Total_stops,
+            'Arrival': Arrival,
+            'Destination': Destination,
+            'Duration_in_hours': Duration_in_hours,
+            'Days_left': Days_left,
+            'Year': Year,
+            'Month': Month,
+            'Day': Day,
+            'Day_of_week': Day_of_week
+            }]
+        data = pd.DataFrame(df, index=[13])
 
 
-        return render_template('home.html',prediction_text="₹ {}".format(output))
+
+
+        # obj= DataTransformation()
+        # prediction=obj.get_data_transformer(Airline,
+        #                                     Classes,
+        #                                     Source, 
+        #                                     Departure,
+        #                                     Total_stops,
+        #                                     Arrival,
+        #                                     Destination,
+        #                                     Duration_in_hours,
+        #                                     Days_left,
+        #                                     Year,
+        #                                     Month,
+        #                                     Day,
+        #                                     Day_of_week)
+        
+        # output=round(prediction[0],2) prediction_text="₹ {}".format(output)
+
+
+        return render_template('home.html',prediction_text=Days_left)
 
 
     return render_template("home.html")
