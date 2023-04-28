998d2939c155dd52039bbdff069ac4517b229500
laiz-in <laizin2107@gmail.com>
2023-04-28 10:56:09 +0400

    edited utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+from logger import logging
 import numpy as np 
 import pandas as pd
 import dill
@@ -12,12 +12,10 @@ from exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
-
+        logging.info("object saved")
     except Exception as e:
         raise CustomException(e, sys)
     
@@ -35,19 +33,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-
             #model.fit(X_train, y_train)  # Train model
-
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
-
             report[list(models.keys())[i]] = test_model_score
-
+        logging.info("model evaluated")
         return report
 
     except Exception as e:
@@ -57,7 +49,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
+            logging.info("object loaded")
             return dill.load(file_obj)
-
+        
     except Exception as e:
         raise CustomException(e, sys)
